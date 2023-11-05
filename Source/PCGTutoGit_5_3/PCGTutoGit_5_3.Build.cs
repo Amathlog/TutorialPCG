1f7134a15d544aee81c1623e8ab1aecb01d4d24b
Adrien Logut <Newsadrien@gmail.com>
2023-11-05 18:44:15 -0500

    Add ConvexHull custom node

--- Source/PCGTutoGit_5_3/PCGTutoGit_5_3.Build.cs
+++ Source/PCGTutoGit_5_3/PCGTutoGit_5_3.Build.cs
@@ -8,6 +8,6 @@ public class PCGTutoGit_5_3 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PCG" });
 	}
 }
